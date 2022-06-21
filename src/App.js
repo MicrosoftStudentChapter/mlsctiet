d5180358929701758c2b805652d71af5213d7e31
destrex271 <destrex271@gmail.com>
2022-06-21 18:54:27 +0530

    Background and Heading elements Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import AboutUs from "./Pages/AboutUs";
-import Alumni from "./Pages/Alumni";
-import Community from "./Pages/Community";
+// import AboutUs from "./Pages/AboutUs";
+// import Alumni from "./Pages/Alumni";
+// import Community from "./Pages/Community";
 import Events from "./Pages/Events";
-import Gallery from "./Pages/Gallery";
-import LandingPage from "./Pages/LandingPage";
-import OurTeam from "./Pages/OurTeam";
-import Projects from "./Pages/Projects";
-import Sponsors from "./Pages/Sponsors";
+// import Gallery from "./Pages/Gallery";
+// import LandingPage from "./Pages/LandingPage";
+// import OurTeam from "./Pages/OurTeam";
+// import Projects from "./Pages/Projects";
+// import Sponsors from "./Pages/Sponsors";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      {/* <LandingPage />
       <AboutUs />
       <OurTeam />
-      <Projects />
+      <Projects /> */}
       <Events />
-      <Gallery />
+      {/* <Gallery />
       <Sponsors />
       <Alumni />
-      <Community />
+      <Community /> */}
     </div>
   );
 }
--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./styles/event.module.css";
 
 const Events = () => {
   return (
-    <div>
-      
+    <div className={styles.body}>
+      <div className={styles.header}>sd</div>
+      <div className={styles.container}>sds</div>
+      <div className={styles.footer}>
+        <div className={styles.footText}>
+          <p className={styles.mainHead}>EVENTS</p>
+          <p className={styles.subHead}>@ MLSC</p>
+        </div>
+      </div>
     </div>
   );
 };
