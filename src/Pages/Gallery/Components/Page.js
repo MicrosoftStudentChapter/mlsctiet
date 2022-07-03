88a860f7f631def453a30a35df01ea66123e7ad3
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2022-07-04 00:59:54 +0530

    changed .css to .module.css

--- src/Pages/Gallery/Components/Page.js
+++ src/Pages/Gallery/Components/Page.js
@@ -1,3 +1,5 @@
+import styles from "../Style/Gallery.module.css"
+
 export default function Page(){
     // JS
       function changeimg(){
@@ -11,13 +13,14 @@ export default function Page(){
           attr2.style.transform="translateX(50vw)";
           attr2.style.filter="blur(10px)";  
         }  
-        if(n==2){
+        else if(n==2){
           attr1.style.transform="translateX(-50vw)";
           attr1.style.filter="blur(10px)";
           attr2.style.transform="translateX(0vw)";
           attr2.style.filter="blur(0px)";
           attr3.style.transform="translateX(50vw)";
           attr3.style.filter="blur(10px)";
+          attr4.style.transform="translateX(-100vw)";
           attr4.style.display="none";
         }
       }
@@ -50,15 +53,15 @@ export default function Page(){
       
     //   JSX
       return(
-        <div className='page-wrapper'>
-            <img id='img1'className='image' src="https://www.pandotrip.com/wp-content/uploads/2013/05/red-beach-china-740x440.jpg"/>
-            <img id='img2'className='image' src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/16/mirador-las-torres-gettyimages-512588114.jpg?crop=1.0xw:1xh;center,top&resize=480:*"/>
-            <img id='img3'className='image' src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/most-beautiful-places-in-the-world-gettyimages-1042549972.jpg"/>
-          <img id='img4'className='image' src="https://media.cntraveler.com/photos/5cb63a4c1a7e7050e8f70959/master/w_4000,h_2667,c_limit/Socotra-Yemen_GettyImages-487703989.jpg"/>
-          <div className="align-center">
-            <button className="btn" onClick={subtract}>&lt; </button>
-              <div id='image-placeholder'></div>
-            <button className="btn" onClick={add}> &gt; </button>
+        <div className={styles.pagewrapper}>
+            <img id={styles.img1} className={styles.image}src="https://www.pandotrip.com/wp-content/uploads/2013/05/red-beach-china-740x440.jpg"/>
+            <img id={styles.img2} className={styles.image} src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/16/mirador-las-torres-gettyimages-512588114.jpg?crop=1.0xw:1xh;center,top&resize=480:*"/>
+            <img id={styles.img3}className={styles.image} src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/most-beautiful-places-in-the-world-gettyimages-1042549972.jpg"/>
+          <img id={styles.img4}className={styles.image} src="https://media.cntraveler.com/photos/5cb63a4c1a7e7050e8f70959/master/w_4000,h_2667,c_limit/Socotra-Yemen_GettyImages-487703989.jpg"/>
+          <div className={styles.aligncenter}>
+            <button className={styles.btn} onClick={subtract}>&lt; </button>
+              <div id={styles.imageplaceholder}></div>
+            <button className={styles.btn} onClick={add}> &gt; </button>
           </div>
         </div>  
       )
--- src/Pages/Gallery/index.js
+++ src/Pages/Gallery/index.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import styles from "./Style/style.css"
-// import Navbar from "../../Components/Navbar.js"
-// import Page from "./Components/Page.js"
+import Page from "./Components/Page.js"
 
 
 
 const Gallery = () => {
   return(
   <div>
-    Gallery
-    {/* <Navbar/>
-    <Page/> */}
+    <Page/>
   </div>
   
   
