da5f6b644c027b7cdf559c456f86cbd38d4afaf6
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2022-07-19 01:35:30 +0530

    Finished basic functionality of first page

--- src/Pages/Gallery/Components/Page.js
+++ src/Pages/Gallery/Components/Page.js
@@ -10,6 +10,10 @@ export default function Page(){
           const attr4=document.querySelector('#img4');
           const attrbtn1=document.getElementById("btn1");
           const attrbtn2=document.getElementById("btn2");
+          const text1=document.querySelector('#twenty19')
+          const text2=document.querySelector('#twenty20')
+          const text3=document.querySelector('#twenty21')
+          const text4=document.querySelector('#twenty22')
           
         if(n==1){
           attr1.style.zIndex="2";
@@ -17,21 +21,27 @@ export default function Page(){
           attr3.style.zIndex="1";
           attr4.style.zIndex="1";
 
+          text1.style.transform="TranslateY(240px)"
+          text2.style.transform="TranslateY(100vh)"
+          text3.style.transform="TranslateY(100vh)"
+          text4.style.transform="TranslateY(100vh)"
+
+
           attr1.style.transform="translateX(0vw)";
-          attr1.style.filter="blur(0px)";
+          attr1.style.filter="blur(0px) saturate(0)";
           // attr1.style.visibility="visible";
           attr1.style.opacity="1"
           attr2.style.transform="translateX(50vw)";
           // attr2.style.visibility="visible";
           attr2.style.opacity="1"
-          attr2.style.filter="blur(10px)";
+          attr2.style.filter="blur(13px) saturate(0)";
           // attr3.style.visibility="hidden";
           attr3.style.opacity="0"
           attr3.style.transform="translateX(-100vw)";
           // attr4.style.visibility="visible";
           attr4.style.opacity="1"
           attr4.style.transform="translateX(-50vw)";
-          attr4.style.filter="blur(10px)";
+          attr4.style.filter="blur(13px) saturate(0)";
           
           setTimeout(() => {
             attr3.style.transform="translateX(100vw)";
@@ -48,15 +58,21 @@ export default function Page(){
           attr3.style.zIndex="1";
           attr4.style.zIndex="1";
 
+          text1.style.transform="TranslateY(100vh)"
+          text2.style.transform="TranslateY(240px)"
+          text3.style.transform="TranslateY(100vh)"
+          text4.style.transform="TranslateY(100vh)"
+
+
           attr1.style.transform="translateX(-50vw)";
-          attr1.style.filter="blur(10px)";
+          attr1.style.filter="blur(13px) saturate(0)";
           // attr1.style.visibility="visible";
           attr1.style.opacity="1"
           attr2.style.transform="translateX(0vw)";
-          attr2.style.filter="blur(0px)";
+          attr2.style.filter="blur(0px) saturate(0)";
           attr3.style.transform="translateX(50vw)";
           attr3.style.opacity="1"
-          attr3.style.filter="blur(10px)";
+          attr3.style.filter="blur(13px) saturate(0)";
           attr4.style.transform="translateX(-100vw)";
           // attr4.style.visibility="hidden";
           attr4.style.opacity="0"
@@ -74,16 +90,21 @@ export default function Page(){
           attr3.style.zIndex="3";
           attr4.style.zIndex="1";
 
+          text1.style.transform="TranslateY(100vh)"
+          text2.style.transform="TranslateY(100vh)"
+          text3.style.transform="TranslateY(240px)"
+          text4.style.transform="TranslateY(100vh)"
+
           // attr2.style.visibility="visible";
           attr2.style.opacity="1"
           attr2.style.transform="translateX(-50vw)";
-          attr2.style.filter="blur(10px)";
+          attr2.style.filter="blur(13px) saturate(0)";
           attr3.style.transform="translateX(0vw)";
-          attr3.style.filter="blur(0px)";
+          attr3.style.filter="blur(0px) saturate(0)";
           // attr4.style.visibility="visible";
           attr4.style.opacity="1"
           attr4.style.transform="translateX(50vw)";
-          attr4.style.filter="blur(10px)";
+          attr4.style.filter="blur(13px) saturate(0)";
           attr1.style.transform="translateX(-100vw)";
           // attr1.style.visibility="hidden";
           attr1.style.opacity="0"
@@ -99,17 +120,22 @@ export default function Page(){
           attr1.style.zIndex="1";
           attr2.style.zIndex="1";
           attr3.style.zIndex="1";
-          attr4.style.zIndex="4";
+          attr4.style.zIndex="2";
+
+          text1.style.transform="TranslateY(100vh)"
+          text2.style.transform="TranslateY(100vh)"
+          text3.style.transform="TranslateY(100vh)"
+          text4.style.transform="TranslateY(240px)"
 
           attr3.style.transform="translateX(-50vw)";
-          attr3.style.filter="blur(10px)";
+          attr3.style.filter="blur(13px) saturate(0)";
           attr4.style.transform="translateX(0vw)";
-          attr4.style.filter="blur(0px)";
+          attr4.style.filter="blur(0px) saturate(0)";
           attr1.style.display="block";
           attr1.style.transform="translateX(50vw)";
           // attr1.style.visibility="visible";
           attr1.style.opacity="1"
-          attr1.style.filter="blur(10px)";
+          attr1.style.filter="blur(13px) saturate(0)";
           attr2.style.transform="translateX(-100vw)";
           // attr2.style.visibility="hidden";
           attr2.style.opacity="0"
@@ -131,6 +157,11 @@ export default function Page(){
         const attr4=document.querySelector('#img4');
         const attrbtn1=document.getElementById("btn1");
         const attrbtn2=document.getElementById("btn2");
+        const text1=document.querySelector('#twenty19')
+        const text2=document.querySelector('#twenty20')
+        const text3=document.querySelector('#twenty21')
+        const text4=document.querySelector('#twenty22')
+          
         
       if(n==1){
         attr1.style.zIndex="2";
@@ -138,21 +169,26 @@ export default function Page(){
         attr3.style.zIndex="1";
         attr4.style.zIndex="1";
 
+        text1.style.transform="TranslateY(240px)"
+        text2.style.transform="TranslateY(100vh)"
+        text3.style.transform="TranslateY(100vh)"
+        text4.style.transform="TranslateY(100vh)"
+
         attr1.style.transform="translateX(0vw)";
-        attr1.style.filter="blur(0px)";
+        attr1.style.filter="blur(0px) saturate(0)";
         // attr1.style.visibility="visible";
         attr1.style.opacity="1"
         attr2.style.transform="translateX(50vw)";
         // attr2.style.visibility="visible";
         attr2.style.opacity="1"
-        attr2.style.filter="blur(10px)";
+        attr2.style.filter="blur(13px) saturate(0)";
         // attr3.style.visibility="hidden";
         attr3.style.opacity="0"
         attr3.style.transform="translateX(-100vw)";
         // attr4.style.visibility="visible";
         attr4.style.opacity="1"
         attr4.style.transform="translateX(-50vw)";
-        attr4.style.filter="blur(10px)";
+        attr4.style.filter="blur(13px) saturate(0)";
         
         setTimeout(() => {
           attr3.style.transform="translateX(100vw)";
@@ -169,15 +205,20 @@ export default function Page(){
         attr3.style.zIndex="1";
         attr4.style.zIndex="1";
 
+        text1.style.transform="TranslateY(100vh)"
+        text2.style.transform="TranslateY(240px)"
+        text3.style.transform="TranslateY(100vh)"
+        text4.style.transform="TranslateY(100vh)"
+
         attr1.style.transform="translateX(-50vw)";
-        attr1.style.filter="blur(10px)";
+        attr1.style.filter="blur(13px) saturate(0)";
         // attr1.style.visibility="visible";
         attr1.style.opacity="1"
         attr2.style.transform="translateX(0vw)";
-        attr2.style.filter="blur(0px)";
+        attr2.style.filter="blur(0px) saturate(0)";
         attr3.style.transform="translateX(50vw)";
         attr3.style.opacity="1"
-        attr3.style.filter="blur(10px)";
+        attr3.style.filter="blur(13px) saturate(0)";
         attr4.style.transform="translateX(-100vw)";
         // attr4.style.visibility="hidden";
         attr4.style.opacity="0"
@@ -194,17 +235,22 @@ export default function Page(){
         attr2.style.zIndex="1";
         attr3.style.zIndex="2";
         attr4.style.zIndex="1";
+        
+        text1.style.transform="TranslateY(100vh)"
+        text2.style.transform="TranslateY(100vh)"
+        text3.style.transform="TranslateY(240px)"
+        text4.style.transform="TranslateY(100vh)"
 
         // attr2.style.visibility="visible";
         attr2.style.opacity="1"
         attr2.style.transform="translateX(-50vw)";
-        attr2.style.filter="blur(10px)";
+        attr2.style.filter="blur(13px) saturate(0)";
         attr3.style.transform="translateX(0vw)";
-        attr3.style.filter="blur(0px)";
+        attr3.style.filter="blur(0px) saturate(0)";
         // attr4.style.visibility="visible";
         attr4.style.opacity="1"
         attr4.style.transform="translateX(50vw)";
-        attr4.style.filter="blur(10px)";
+        attr4.style.filter="blur(13px) saturate(0)";
         attr1.style.transform="translateX(-100vw)";
         // attr1.style.visibility="hidden";
         attr1.style.opacity="0"
@@ -222,16 +268,21 @@ export default function Page(){
         attr3.style.zIndex="1";
         attr4.style.zIndex="2";
 
+        text1.style.transform="TranslateY(100vh)"
+        text2.style.transform="TranslateY(100vh)"
+        text3.style.transform="TranslateY(100vh)"
+        text4.style.transform="TranslateY(240px)"
+
         attr3.style.transform="translateX(-50vw)";
-        attr3.style.filter="blur(10px)";
+        attr3.style.filter="blur(13px) saturate(0)";
         attr3.style.opacity="1"
         attr4.style.transform="translateX(0vw)";
-        attr4.style.filter="blur(0px)";
+        attr4.style.filter="blur(0px) saturate(0)";
         attr1.style.display="block";
         attr1.style.transform="translateX(50vw)";
         // attr1.style.visibility="visible";
         attr1.style.opacity="1"
-        attr1.style.filter="blur(10px)";
+        attr1.style.filter="blur(13px) saturate(0)";
         attr2.style.transform="translateX(-100vw)";
         // attr2.style.visibility="hidden";
         attr2.style.opacity="0"
@@ -286,21 +337,164 @@ export default function Page(){
         }
       }
       
+      const changeimgphn = () => {
+        const attr1=document.querySelector('#img1');
+        const attr2=document.querySelector('#img2');
+        const attr3=document.querySelector('#img3');
+        const attr4=document.querySelector('#img4');
+        const text1=document.querySelector('#twenty19')
+        const text2=document.querySelector('#twenty20')
+        const text3=document.querySelector('#twenty21')
+        const text4=document.querySelector('#twenty22')
+        if(n==1){
+          attr1.style.zIndex="2";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="1";
+
+          attr1.style.opacity="1";
+          attr2.style.opacity="0";
+          attr3.style.opacity="0";
+          attr4.style.opacity="0";
+
+          text1.style.opacity="1";
+          text2.style.opacity="0";
+          text3.style.opacity="0";
+          text4.style.opacity="0";
+        }
+        else if(n==2){
+          attr1.style.zIndex="1";
+          attr2.style.zIndex="2";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="1";
+
+          attr1.style.opacity="0";
+          attr2.style.opacity="1";
+          attr3.style.opacity="0";
+          attr4.style.opacity="0";
+
+          text1.style.opacity="0";
+          text2.style.opacity="1";
+          text3.style.opacity="0";
+          text4.style.opacity="0";
+        }
+        else if(n==3){
+          attr1.style.zIndex="1";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="2";
+          attr2.style.zIndex="1";
+
+          attr1.style.opacity="0";
+          attr2.style.opacity="0";
+          attr3.style.opacity="1";
+          attr4.style.opacity="0";
+
+          text1.style.opacity="0";
+          text2.style.opacity="0";
+          text3.style.opacity="1";
+          text4.style.opacity="0";
+        }
+        else if(n==4){
+          attr1.style.zIndex="1";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="1";
+          attr2.style.zIndex="2";
+
+          attr1.style.opacity="0";
+          attr2.style.opacity="0";
+          attr3.style.opacity="0";
+          attr4.style.opacity="1";
+
+          text1.style.opacity="0";
+          text2.style.opacity="0";
+          text3.style.opacity="0";
+          text4.style.opacity="1";
+        }
+      }
+
+      const addphn = () => {
+        if(n<4){
+          n++;
+          console.log(n);
+          changeimgphn();
+        }
+        else{
+          n=1;
+          console.log(n);
+          changeimgphn();
+        }
+      }
+      const subtractphn =() =>{
+        if(n>1){
+          n--;
+          console.log(n);
+          changeimgphn();
+        }
+        else{
+          n=4;
+          console.log(n);
+          changeimgphn();
+        }
+      }
+
+      let imgobject=document.querySelector('.image')
+
+            const mousehover = () =>{
+              const attr1=document.querySelector('#img1');
+              const attr2=document.querySelector('#img2');
+              const attr3=document.querySelector('#img3');
+              const attr4=document.querySelector('#img4');
+                      if(n==1){
+                        attr1.style.filter="blur(0px) saturate(100%)";
+                      }
+                      else if(n==2){
+                        attr2.style.filter="blur(0px) saturate(100%)";
+                      }
+                      else if(n==3){
+                        attr3.style.filter="blur(0px) saturate(100%)";
+                      }
+                      else if(n==4){
+                        attr4.style.filter="blur(0px) saturate(100%)";
+                      }
+            }
+            const unhover = () =>{
+              const attr1=document.querySelector('#img1');
+              const attr2=document.querySelector('#img2');
+              const attr3=document.querySelector('#img3');
+              const attr4=document.querySelector('#img4');
+                      if(n==1){
+                        attr1.style.filter="blur(0px) saturate(0)";
+                      }
+                      else if(n==2){
+                        attr2.style.filter="blur(0px) saturate(0)";
+                      }
+                      else if(n==3){
+                        attr3.style.filter="blur(0px) saturate(0)";
+                      }
+                      else if(n==4){
+                        attr4.style.filter="blur(0px) saturate(0)";
+                      }
+            }
+
     //   JSX
       return(
         <div className="pagewrapper">
             {/* try image left and right technique */}
-          <div className="aligncenter">
-            <img id="img1" className="image"src="https://www.pandotrip.com/wp-content/uploads/2013/05/red-beach-china-740x440.jpg"/>
-            <img id="img2" className="image" src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/16/mirador-las-torres-gettyimages-512588114.jpg?crop=1.0xw:1xh;center,top&resize=480:*"/>
-            <img id="img3"className="image" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/most-beautiful-places-in-the-world-gettyimages-1042549972.jpg"/>
-            <img id="img4"className="image" src="https://media.cntraveler.com/photos/5cb63a4c1a7e7050e8f70959/master/w_4000,h_2667,c_limit/Socotra-Yemen_GettyImages-487703989.jpg"/>
-
+            <img id="img1" className="image" onMouseOver={mousehover} onMouseOut={unhover} src="https://www.pandotrip.com/wp-content/uploads/2013/05/red-beach-china-740x440.jpg"/>
+            <img id="img2" className="image" onMouseOver={mousehover} onMouseOut={unhover} src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/16/mirador-las-torres-gettyimages-512588114.jpg?crop=1.0xw:1xh;center,top&resize=480:*"/>
+            <img id="img3"className="image" onMouseOver={mousehover} onMouseOut={unhover} src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/most-beautiful-places-in-the-world-gettyimages-1042549972.jpg"/>
+            <img id="img4"className="image" onMouseOver={mousehover} onMouseOut={unhover} src="https://media.cntraveler.com/photos/5cb63a4c1a7e7050e8f70959/master/w_4000,h_2667,c_limit/Socotra-Yemen_GettyImages-487703989.jpg"/>
             
+            <h1 id="twenty19" className="YearTags">2019</h1>
+            <h1 id="twenty20" className="YearTags">2020</h1>
+            <h1 id="twenty21" className="YearTags">2021</h1>
+            <h1 id="twenty22" className="YearTags">2022</h1>
+          <div className="aligncenter">
+
             <button id='btn1' className="btn" onClick={subtract}>&lt; </button>
-            <button id='btn1phn' className="btn" onClick={subtract}>&lt; </button>
+            <button id='btn1phn' className="btn" onClick={subtractphn}>&lt; </button>
               <div id="imageplaceholder"></div>
-            <button id='btn2phn' className="btn" onClick={add}>&gt; </button>
+            <button id='btn2phn' className="btn" onClick={addphn}>&gt; </button>
             <button id='btn2' className="btn" onClick={add}> &gt; </button>
           </div>
         </div>  
