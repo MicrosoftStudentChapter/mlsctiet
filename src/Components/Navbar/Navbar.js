4453d1762518d91e35ab824b7f1a7ec96f100bcf
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2022-08-25 16:44:32 +0530

    added hamburger with useState

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import logo from '../../Assets/mlsc_shield_new.png'
 import './Navbar.css'
+import {HiOutlineMenu} from 'react-icons/hi'
+import { useState } from 'react'
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-
+  const [open, setOpen] = useState(false)
   return (
     <>
     <nav>
+      {/* Hamburger displayed with media query */}
+      <div className='hamburger'>
+        <HiOutlineMenu size="40px"
+        onClick={() =>{
+          setOpen(!open);
+          console.log(open);
+        } 
+        }
+        />
+        
+      </div>
+
       {/* NavBar MLSC logo */}
       <div className="left-nav">
         <Link to="/">
