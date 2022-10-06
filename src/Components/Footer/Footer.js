45420895a20f420b9ad08b1594fa6b27cd374482
Shivansh Kapoor <kapoor.shivansh2002@gmail.com>
2022-10-07 01:20:18 +0530

    footer boilerplate

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles/footer.module.css";
+const Footer = () => {
+
+    return(
+        <React.Fragment>
+            <div className={styles.Footer}>
+
+            </div>
+        </React.Fragment>
+
+    );
+}
+
+export default Footer;
