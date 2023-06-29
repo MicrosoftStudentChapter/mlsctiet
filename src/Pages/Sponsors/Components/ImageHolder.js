fd68298b357d5cdde2f388ccd913f2872178cb6c
Siddharth Aggarwal <saggarwal_be22@thapar.edu>
2023-06-29 19:29:08 +0530

    commented

--- src/Pages/Sponsors/Components/ImageHolder.js
+++ src/Pages/Sponsors/Components/ImageHolder.js
@@ -2,6 +2,7 @@ import react from "react";
 import styles from "../styles/home.module.css";
 import circle from "../assets/circle.png";
 
+//hello can you guys see this comment
 const ImageHolder = ({data}) => {
 	return (
 				<div className={styles.container1}>
