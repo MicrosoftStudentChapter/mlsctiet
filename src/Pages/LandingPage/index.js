36f403ff7805e5dafc1238bdb431ee1f535067ff
Jayita Lakhmani <jlakhmani_be22@thapar.edu>
2023-07-01 04:28:54 +0530

    landing page changes

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles/home.module.css";
 import sheild from "./mlsc-logo.png";
+import bottom from "./Bottom.png";
 
 const LandingPage = () => {
   return (
@@ -9,15 +10,16 @@ const LandingPage = () => {
       <div className={styles.left}>
         <div className={styles.polygon}>
           <img src={sheild} className={styles.sheild} alt="Sheild" />
+          <img src={bottom} className={styles.bottom} alt="Bottom" />
           
-          <p className={styles.mobileTitle}>MICROSOFT LEARN STUDENT CHAPTER</p>
+          {/* <p className={styles.mobileTitle}>MICROSOFT LEARN STUDENT CHAPTER</p> */}
         </div>
       </div>
 
       <div className={styles.right}>
-        <div className={styles.microsoft}>MICROSOFT</div>
+        {/* <div className={styles.microsoft}>MICROSOFT</div>
         <div className={styles.learn}>LEARN STUDENT</div>
-        <div className={styles.chapter}>CHAPTER</div>
+        <div className={styles.chapter}>CHAPTER</div> */}
       </div>
  
     </div>
