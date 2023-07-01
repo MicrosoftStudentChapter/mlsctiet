11e7e2d20c1f8da210f1270193455ab3ddc25df4
palak <palakpreetkaur01052004@gmail.com>
2023-07-02 00:15:36 +0530

    Initial commit

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./styles/home.module.css";
 import sheild from "./mlsc-logo.png";
 import bottom from "./Bottom.png";
+import content from "./mlsc.png";
+import stroke from "./mlsc-stroke.png";
+import blue from "./mlsc-blue-stroke.png";
 
 const LandingPage = () => {
   return (
@@ -10,7 +13,12 @@ const LandingPage = () => {
       <div className={styles.left}>
         <div className={styles.polygon}>
           <img src={sheild} className={styles.sheild} alt="Sheild" />
+          <img src={blue} className={styles.blue} alt="blue-stroked-mlsc"/>
+          <img src={stroke} className={styles.stroke} alt="stroked-mlsc"/>
+          <img src={content} className={styles.content} alt="mlsc" />
           <img src={bottom} className={styles.bottom} alt="Bottom" />
+          
+
           
           {/* <p className={styles.mobileTitle}>MICROSOFT LEARN STUDENT CHAPTER</p> */}
         </div>
@@ -20,6 +28,7 @@ const LandingPage = () => {
         {/* <div className={styles.microsoft}>MICROSOFT</div>
         <div className={styles.learn}>LEARN STUDENT</div>
         <div className={styles.chapter}>CHAPTER</div> */}
+    
       </div>
  
     </div>
