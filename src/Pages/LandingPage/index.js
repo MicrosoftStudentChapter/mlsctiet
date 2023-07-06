9beaefed3fd4757e9815e2cb27f5ac0a858f20c1
Mankirat Singh <msingh2_be22@thapar.edu>
2023-07-06 12:29:37 +0530

    Stars bg added and responsiveness of synthwave enhanced
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from "./styles/home.module.css";
-import sheild from "./mlsc-logo.png";
-import bottom from "./Bottom.png";
-import content from "./mlsc.png";
-import stroke from "./mlsc-stroke.png";
-import blue from "./mlsc-blue-stroke.png";
-import udlogo from "./udlogo.png";
+import sheild from "./Assets/mlsc-logo.png";
+import content from "./Assets/Desktop/mlsc.png";
+import stroke from "./Assets/Desktop/mlsc-stroke.png";
+import blue from "./Assets/Desktop/mlsc-blue-stroke.png";
 import SynthWave from "./Components/synthwave"
+import StarsCanvas from "./Components/Stars";
 
 
 const LandingPage = () => {
   return (
-    
+    <>
+    <div className={styles.stars}>
+    <StarsCanvas/>
+    </div>
     <div className={styles.container}>
       <div className={styles.left}>
           <div className={styles.polygon}>
@@ -21,14 +23,13 @@ const LandingPage = () => {
           <img src={stroke} className={styles.stroke} alt="stroked-mlsc"/>
           <img src={content} className={styles.content} alt="mlsc"/>
           </div>
-          {/* <img src={udlogo} className={styles.udlogo} alt="Upside Down Logo" /> */}
           <img src={sheild} className={styles.udlogo} alt="Upside Down Logo" />
          </div> 
       </div>
       
       <SynthWave/>
     </div>
-   
+    </>
     
   );
 };
