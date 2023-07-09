0b2241e8bf113b87b8a7bb834b9c9ed73033fe28
adityacodes30 <adityaework@gmail.com>
2023-07-09 13:17:42 +0530

    navbar issue fixed

--- src/Pages/Gallery/Components/Page.js
+++ src/Pages/Gallery/Components/Page.js
@@ -3,15 +3,17 @@ import i2022 from './2022';
 import ExpandableCard from './expandableCard'
 import p2019 from './2019';
 import p2021 from './2021';
+import classes from './page.module.css'
+
 
 function Page() {
   
   const [active, setActive] = useState(null)
   
-  
+
   return (
 
-      <div style={{position:'relative', top:'30vh' , zIndex:'2'}}>
+      <div className={classes.primary}>
       <ExpandableCard year="2022" index={0} activity={setActive} actid = {active} img={i2022} ></ExpandableCard>
       <ExpandableCard year="2021" index={1} activity={setActive} actid = {active} img={p2021} ></ExpandableCard>
       <ExpandableCard year="2019" index={2} activity={setActive} actid = {active} img={p2019} ></ExpandableCard>
