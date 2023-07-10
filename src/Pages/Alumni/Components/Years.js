f6addfc08647e18f053254546f488c00016d431e
Rijul Bansal <84855217+RijulBansal@users.noreply.github.com>
2023-07-11 00:08:47 +0530

    Update Years.js

--- src/Pages/Alumni/Components/Years.js
+++ src/Pages/Alumni/Components/Years.js
@@ -1,37 +1,7 @@
-// import React from "react";
-// import StyleButton from "../Styles/Years.module.css";
-// import Polaroid from "./Polaroid";
-// import { useNavigate } from "react-router-dom";
-
-// const Years = ({ filterPolaroids, yearItems }) => {
-//   const Color = ["#FFF6EB", "#FA3966", "#0078D4"];
-//   const navigate = useNavigate();
-//   return (
-//     <div className="yearbutton">
-//       {yearItems.map((curElem, index) => {
-//         return (
-//           <button
-//             className={StyleButton.buttons}
-//             key={index}
-//             onClick={() => {
-//               filterPolaroids(curElem);
-//               navigate(`/alumni/${curElem}`);
-//             }}
-//           >
-//             {curElem}
-//           </button>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default Years;
 
 import React from "react";
 import StyleButton from "../Styles/Years.module.css";
 import { useNavigate } from "react-router-dom";
-// import styles from "../Styles/PolaroidGrid.module.css"
 
 const Years = ({ filterPolaroids, yearItems }) => {
   const Color = ["#FFF6EB", "#FA3966", "#0078D4"];
