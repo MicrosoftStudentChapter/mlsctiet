059793e39e93e992ad478f909e9c573bbe797828
Lakshay Singla <lsingla1_be23@thapar.edu>
2024-07-01 23:16:59 +0530

    Background complete

--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,10 +1,9 @@
+import About from "./components/bg"; 
+
 export default function AboutUs(){
     return (
         <div>
-        <h1>About Us</h1>
-        <p>
-            This is the about us page. This is where we talk about our organization.
-        </p>
+            <About />
         </div>
     );
 }
