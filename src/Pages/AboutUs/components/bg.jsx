d073418d40803504f29dc059359df2d1190afd04
Lakshay Singla <lsingla1_be23@thapar.edu>
2024-07-02 19:29:47 +0530

    bg of about page complete

--- src/Pages/AboutUs/components/bg.jsx
+++ src/Pages/AboutUs/components/bg.jsx
@@ -1,18 +1,33 @@
 import style from "./bg.module.css";
+// import "@fontsource/dm-mono";
+// import "@fontsource/dm-mono/400.css";
+// import "@fontsource/dm-mono/400-italic.css";
+
 
 
 export default function About() {
     return (
         <>
             {/* <div className={style.heading}></div> */}
-            <p className={style.text}>A Legacy of Innovation</p>
+            <p className={style.text}>A legacy of innovation</p>
             <div className={style.parent}>
+                <div className={style.triangle}></div>
+                <div className={style.icon}>
+                    <div className={style.arrow}></div>
+                </div>
+                <div className={style.bluetext}>
+                    <p >
+                        A legacy of innovation
+                    </p>
+                </div>
                 <div className={style.left}>
+                    
                     <div className={style.rotatedTextContainer}>
-                        <p className={style.rotatedText}>A Legacy of Innovation</p>
+                        <p className={style.rotatedText}>A legacy of innovation</p>
                     </div>
                     <div className={style.hor}>
                         <div className={style.h1}></div>
+
                         <div>
                             <div className={style.h2}></div>
                             <div className={style.h3}></div>
