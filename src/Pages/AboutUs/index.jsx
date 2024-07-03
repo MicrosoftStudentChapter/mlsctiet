f0013f95f73c835b611d087def75b64c0d5606ca
vishnu <142923987+vishnu1345@users.noreply.github.com>
2024-07-03 13:29:03 +0530

    projects page created

--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,10 +1,9 @@
+import Projects from "./components/projects";
+
 export default function AboutUs(){
     return (
         <div>
-        <h1>About Us</h1>
-        <p>
-            This is the about us page. This is where we talk about our organization.
-        </p>
+        <Projects/>
         </div>
     );
 }
