38e6b084373c472d441b9551af887bcd7bd13874
mannat04 <miglani.mannat2511@gmail.com>
2024-07-06 23:19:11 +0530

    helu

--- src/Pages/Gallery/components/makethon.jsx
+++ src/Pages/Gallery/components/makethon.jsx
@@ -10,7 +10,7 @@ const Makeathon = () => {
       
         <div className={styles.page}>
           <div className={styles.content}>
-            <div>
+            <div className={styles.parent1}>
             <h1 className={styles.title}>Makeathon 6</h1>
             <p className={styles.about1}>
               <span className={styles.aboutTitle}>(about)</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
@@ -22,22 +22,22 @@ const Makeathon = () => {
             </div>
           </div>
 
-          <div className={styles.content}>
+          <div className={styles.content2}>   
             <div className={styles.imageContainer2}>
               <div className={styles.border2}></div>
               <img src={groupImage} alt="Group" className={styles.image2} />         
             </div>
-            <div>
-            <h1 className={styles.title2}>Makeathon 5</h1>
-            <p className={styles.about2}>
-              <span className={styles.aboutTitle}>(about)</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
-            </p>
+            <div className={styles.parent2}>
+              <h1 className={styles.title2}>Makeathon 5</h1>
+              <p className={styles.about2}>
+                <span className={styles.aboutTitle}>(about)</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
+              </p>
             </div>
           </div>
 
 
           <div className={styles.content}>
-            <div>
+            <div className={styles.parent3}>
             <h1 className={styles.title}>Makeathon 4</h1>
             <p className={styles.about1}>
               <span className={styles.aboutTitle}>(about)</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
