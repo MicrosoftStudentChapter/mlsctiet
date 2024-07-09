55fdfd6640008f92c4096748152ff1b773fa76af
Purnika Malhotra <pmalhotra1_be23@thapar.edu>
2024-07-09 19:19:57 +0530

    grid added

--- src/Pages/Alumni/Components/ScrollCenter.jsx
+++ src/Pages/Alumni/Components/ScrollCenter.jsx
@@ -7,6 +7,7 @@ function ScrollCenter({ currentYear }) {
         <div className={styles.start}>
 
           <div className={styles.meow}>
+            <div className={styles.image}><img src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/Documents/MLSC%20PICS%2023-24/Induction%20Day/PXL_20231104_152010834.MP.jpg?csf=1&web=1&e=GA5k9h&CID=7b1cec33-8e76-4647-9568-555dc9e150b5"></img></div>
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
@@ -14,8 +15,7 @@ function ScrollCenter({ currentYear }) {
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
             <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
-            <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
-            <div className={styles.image}><img src="https://t4.ftcdn.net/jpg/05/62/78/85/360_F_562788544_gBBydDZ7jt9eYjTmsXhsXVzPjL2yQfsg.jpg"></img></div>
+            <div className={styles.image}><img src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Ftmann_be22_thapar_edu%2FDocuments%2FMLSC%20PICS%2023-24%2FScribble%20Day%2FIMG_3717.jpeg&parent=%2Fpersonal%2Ftmann_be22_thapar_edu%2FDocuments%2FMLSC%20PICS%2023-24%2FScribble%20Day"></img></div>
           </div>
           <div className={styles.Alumnicap}>
           <div className={styles.vl}></div>
