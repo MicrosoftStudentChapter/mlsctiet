cbba2720dd5e086a42acfc649be7fcb362c81ed2
Lakshay Singla <lsingla1_be23@thapar.edu>
2024-07-09 19:26:01 +0530

    Working on Respnsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import NavbarI from "./Components/NavbarI/NavbarI";
 function App() {
   return (
     <Router>
-      <NavbarI />
+      {/* <NavbarI /> */}
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/events" element={<Events />} />
