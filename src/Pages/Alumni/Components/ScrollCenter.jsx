58afc075baa5930d7a76f39b98fc3c0d6a3b5c15
Akshat <akshatiscoding@gmail.com>
2024-07-11 13:19:52 +0530

    akshat cards

--- src/Pages/Alumni/Components/ScrollCenter.jsx
+++ src/Pages/Alumni/Components/ScrollCenter.jsx
@@ -1,10 +1,31 @@
 import styles from "../Style/scroll.module.css";
+import Cards from "./Card";
+
+const cards = [
+    { title: 'Akshat Nathani', copy: 'Core Member', button: 'facebook', image: '1517021897933-0e0319cfbc28' },
+    { title: 'Preetinder', copy: 'Core Member', button: 'View Trips', image: '1533903345306-15d1c30952de' },
+    { title: 'Desert Destinations', copy: 'It\'s the desert you\'ve always dreamed of', button: 'Book Now', image: '1545243424-0ce743321e11' },
+    { title: 'Explore The Galaxy', copy: 'Seriously, straight up, just blast off into outer space today', button: 'Book Now', image: '1531306728370-e2ebd9d7bb99' }
+  ];
 
 function ScrollCenter({ currentYear }) {
+  
   return (
     <>
      <div className={styles.scrollDiv}>
-     <div className={styles.start}></div>
+     <div className={styles.start}>
+      <div className={styles.grid}>
+      {cards.map((card, index) => (
+        <Cards
+          key={index}
+          title={card.title}
+          copy={card.copy}
+          button={card.button}
+          image={card.image}
+        />
+      ))}
+      </div>
+     </div>
       {currentYear === 2019 && (
         <div>
           2019
