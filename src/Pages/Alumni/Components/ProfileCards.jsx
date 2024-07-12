c09ca9ef07f1889340d6273669e6651b21664846
Preetinder Singh <preetindersingh13per@gmail.com>
2024-07-12 22:44:54 +0530

    Updated  alumni cards

--- src/Pages/Alumni/Components/ProfileCards.jsx
+++ src/Pages/Alumni/Components/ProfileCards.jsx
@@ -7,7 +7,7 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/Documents/MLSC%20PICS%2023-24/Soc%20fair%20Hard%20Creativity/IMG_6487.jpeg?csf=1&web=1&e=VBLXpu&CID=3bd1190b-56c0-433e-a19a-2a2909b267c7"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/c_crop,g_face,h_1.00,w_0.50/alumni_page/2024/gzqbeg8c5ygut0tfywqs.jpg"
             alt="didi"
           />
         </div>
@@ -18,7 +18,18 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/Documents/MLSC%20PICS%2023-24/Soc%20fair%20Hard%20Creativity/IMG_6487.jpeg?csf=1&web=1&e=VBLXpu&CID=3bd1190b-56c0-433e-a19a-2a2909b267c7"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785085/alumni_page/2024/o5y75rjcq1juzhynbcbd.png"
+            alt="didi"
+          />
+        </div>
+        <div className={styles.content}>
+          <h2>Apoorva Didi<br/><span>Gen Sec</span></h2>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/c_crop,g_center,h_1.00,w_0.70/alumni_page/2024/fkxlme1b2ts5mb2mho44.jpg"
             alt="Dolly Seth"
           />
         </div>
@@ -29,7 +40,18 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785210/alumni_page/2024/pmwm7fcdshkkrszyuqo6.png"
+            alt="Dolly Seth"
+          />
+        </div>
+        <div className={styles.content}>
+          <h2>Dolly Seth<br/><span>Digital Marketing</span></h2>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785464/alumni_page/2024/m2xkzlnpv086y76exfs9.jpg"
             alt="Aakash Agrawal"
           />
         </div>
@@ -40,7 +62,7 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785023/alumni_page/2024/swzvmhkxajgcftigymaz.png"
             alt="Aakash Agrawal"
           />
         </div>
@@ -51,7 +73,7 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785131/alumni_page/2024/nggv2jcwaj7emzvurmet.png"
             alt="Aakash Agrawal"
           />
         </div>
@@ -62,7 +84,18 @@ const ProfileCards = () => {
       <div className={styles.box}>
         <div className={styles.imgBox}>
           <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785160/alumni_page/2024/tmja3fgainncgkqvkpht.png"
+            alt="Aakash Agrawal"
+          />
+        </div>
+        <div className={styles.content}>
+          <h2>Aakash Agrawal<br/><span>Chartered Accountant C.A</span></h2>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.imgBox}>
+          <img
+            src="https://res.cloudinary.com/dy1pmolax/image/upload/v1720785366/alumni_page/2024/pmnctbuxvmw6u5ekpbav.jpg"
             alt="Aakash Agrawal"
           />
         </div>
