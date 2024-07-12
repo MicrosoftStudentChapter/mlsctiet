5803d1fc164dc6c2dafe67a1b2fb46a33396e127
Akshat <akshatiscoding@gmail.com>
2024-07-12 22:59:22 +0530

    cards edited

--- src/Pages/Alumni/Components/ProfileCards.jsx
+++ src/Pages/Alumni/Components/ProfileCards.jsx
@@ -1,75 +1,27 @@
 import React from 'react';
 import styles from '../Style/card.module.css';
+import profiles from '../data/profiles.json'; // Assuming profiles.json is in the data folder
 
 const ProfileCards = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/Documents/MLSC%20PICS%2023-24/Soc%20fair%20Hard%20Creativity/IMG_6487.jpeg?csf=1&web=1&e=VBLXpu&CID=3bd1190b-56c0-433e-a19a-2a2909b267c7"
-            alt="didi"
-          />
+      {profiles.map((profile, index) => (
+        <div className={styles.box} key={index}>
+          <div className={styles.imgBox}>
+            <img src={profile.image} alt={profile.alt} />
+          </div>
+          <div className={styles.content}>
+            <h2>
+              {profile.name}
+              <a href={profile.linkedin} target="_blank" rel="noopener noreferrer" className={styles.linkedin}>
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" />
+              </a>
+              <br />
+              <span>{profile.role}</span>
+            </h2>
+          </div>
         </div>
-        <div className={styles.content}>
-          <h2>Apoorva Didi<br/><span>Gen Sec</span></h2>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://thaparuniv-my.sharepoint.com/personal/tmann_be22_thapar_edu/Documents/MLSC%20PICS%2023-24/Soc%20fair%20Hard%20Creativity/IMG_6487.jpeg?csf=1&web=1&e=VBLXpu&CID=3bd1190b-56c0-433e-a19a-2a2909b267c7"
-            alt="Dolly Seth"
-          />
-        </div>
-        <div className={styles.content}>
-          <h2>Dolly Seth<br/><span>Digital Marketing</span></h2>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
-            alt="Aakash Agrawal"
-          />
-        </div>
-        <div className={styles.content}>
-          <h2>Aakash Agrawal<br/><span>Chartered Accountant C.A</span></h2>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
-            alt="Aakash Agrawal"
-          />
-        </div>
-        <div className={styles.content}>
-          <h2>Aakash Agrawal<br/><span>Chartered Accountant C.A</span></h2>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
-            alt="Aakash Agrawal"
-          />
-        </div>
-        <div className={styles.content}>
-          <h2>Aakash Agrawal<br/><span>Chartered Accountant C.A</span></h2>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg"
-            alt="Aakash Agrawal"
-          />
-        </div>
-        <div className={styles.content}>
-          <h2>Aakash Agrawal<br/><span>Chartered Accountant C.A</span></h2>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
